fb25ae7e1bb9299e774cdb2324454d7e63727e38
Dominic Faryna <dominic.faryna@gmail.com>
2016-11-20 00:13:14 -0500

    moved scan and movement of teh slide to their own independant functions

--- Program.c
+++ Program.c
@@ -1,32 +1,92 @@
 #include "NXT_FileIO.c"
 
 void homeMotorB();
+void moveMotorB(int toDist);
+
+struct Skittle{
+	int normal;
+	int R;
+	int G;
+	int B;
+}
 
 struct A2D{
 	int array[4][4];
-
 }
 
 struct A1D{
 	int array[4];
 }
+void config(Skittle returnSkittle){
 
-void config(){
 	int raw[4][3];
 	for(int i = 0; i < 4; i++){
-		for(int j < 0; j < 3; j++){
+		for(int j = 0; j < 3; j++){
 			raw[i][j] = 0;
 		}
 	}
 
+}
+
+void spitSkittle(Skittle read){
 
 
+		if(read.normal == 5 && read.R >= 40){
+			eraseDisplay();
+			displayString(5, "ORANGE");
+			moveMotorB(90);
+		}
+		else if(read.normal == 5 && read.R >= 30 && read.R < 40){
+			eraseDisplay();
+			displayString(5, "RED");
+			moveMotorB(110);
+		}
+		else if((read.normal == 5 && read.R < 30) || read.R == 1){
+			eraseDisplay();
+			displayString(5, "PURPLE");
+			moveMotorB(5);
+		}
+		else if(read.normal == 4 && read.R >= 40){
+			eraseDisplay();
+			displayString(0, "YELLOW");
+			moveMotorB(35);
+	}
+	else if((read.normal == 4 && read.R > 20 && read.R < 40) || (read.normal == 3)){
+			eraseDisplay();
+			displayString(5, "GREEN");
+			moveMotorB(60);
+	}else
+	{
+		eraseDisplay();
+		displayString(5, "I'm Confused");
+	}
 }
 
 task doit(){
 	wait1Msec(200);
 	homeMotorB();
 }
+
+void scanForColour(Skittle read){
+		wait10Msec(10);
+		motor[motorC] = 0;
+		SensorType[S3] = sensorColorNxtRED;
+		wait1Msec(1000);
+		read.R = SensorValue[S3];
+
+		SensorType[S3] = sensorColorNxtGREEN;
+		wait1Msec(1000);
+		read.G = SensorValue[S3];
+		SensorType[S3] = sensorColorNxtBLUE;
+		wait1Msec(1000);
+		read.B = SensorValue[S3];
+
+		SensorType[S3] = sensorColorNxtFULL;
+		wait1Msec(1000);
+		read.normal = SensorValue[S3];
+
+}
+
 void moveMotorB(int toDist){
 	nMotorEncoder[motorB] = 0;
 	motor[motorB] = 10;
@@ -43,7 +103,7 @@ void homeMotorB(){
 
 void testMotorC(){
 	int go = -15;
-	int normal = 0, R=0, G=0, B=0;
+	Skittle colour;
 	while(1){
 	motor[motorC] = go;
 	if(SensorValue[S1] == 1){
@@ -61,64 +121,16 @@ void testMotorC(){
 	}
 
 	if(SensorValue[S3] != 6 && go > 0){
-		wait10Msec(10);
-		motor[motorC] = 0;
-		normal = sensorValue[S3];
-		sensorType[S3] = sensorColorNxtRED;
-		wait1Msec(1000);
-		R = sensorValue[S3];
-
-		sensorType[S3] = sensorColorNxtGREEN;
-		wait1Msec(1000);
-		G = sensorValue[S3];
-		sensorType[S3] = sensorColorNxtBLUE;
-		wait1Msec(1000);
-		B = sensorValue[S3];
-
+		wait1Msec(100);
+		if(SensorValue[S3] != 6)	scanForColour(colour);
 		go = 5;
-		sensorType[S3] = sensorColorNxtFULL;
-		wait1Msec(1000);
-		normal = sensorValue[S3];
 
-		if(normal == 5 && R >= 40){
-			eraseDisplay();
-			displayString(5, "ORANGE");
-			moveMotorB(90);
-		}
-		else if(normal == 5 && R >= 30 && R < 40){
-			eraseDisplay();
-			displayString(5, "RED");
-			moveMotorB(110);
-		}
-		else if((normal == 5 && R < 30) || R == 1){
-			eraseDisplay();
-			displayString(5, "PURPLE");
-			moveMotorB(5);
-		}
-		else if(normal == 4 && R >= 40){
-			eraseDisplay();
-			displayString(0, "YELLOW");
-			moveMotorB(35);
-	}
-	else if((normal == 4 && R > 20 && R < 40) || (normal == 3)){
-			eraseDisplay();
-			displayString(5, "GREEN");
-			moveMotorB(60);
-	}else
-	{
+ 		spitSkittle(colour);
 		eraseDisplay();
-		displayString(5, "I'm Confused");
-	}
-
-		eraseDisplay();
-		writeDebugStreamLine("%i", R);
-		writeDebugStreamLine("%i", G);
-		writeDebugStreamLine("%i", B);
-		writeDebugStreamLine("%i", normal);
-		displayString(0, "%i", R);
-		displayString(1, "%i", G);
-		displayString(2, "%i", B);
-		displayString(3, "%i", normal);
+		displayString(0, "%i", colour.R);
+		displayString(1, "%i", colour.G);
+		displayString(2, "%i", colour.B);
+		displayString(3, "%i", colour.normal);
 
 		motor[motorC] = go;
 		wait1Msec(500);
@@ -179,9 +191,9 @@ task main()
 
 
 	//displayStatus();
-	config();
+	//config();
 
-	//testMotorC();
+	testMotorC();
 
 
 }
