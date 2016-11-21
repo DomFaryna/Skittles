4df9f35d1ce58408bb3c629cc122e299b8bb2ed6
Dominic Faryna <dominic.faryna@gmail.com>
2016-11-21 09:12:53 -0500

    added a total value to skittles. Added a few comments here and there

--- Program.c
+++ Program.c
@@ -1,14 +1,15 @@
 #include "NXT_FileIO.c"
 
-struct Skittle{	// a struct used to
-	int normal;
-	int R;
-	int G;
-	int B;
+struct Skittle{	// a struct used to store skittle information
+	int normal; // The colour the nxt thinks the skittle is. E.g. 5 = red, 4 = yellow, 1 = black, etc
+	int R; // the R value of the skittle
+	int G; // the G value of the skittle
+	int B; // the B value of the skittle
+	int sum; // The sum of the RGB values
 }
 
 struct A2D{		// a sturct used to create and pass 2D arrays throughout the rest of the program
-	int array[5][10];
+	int array[5][13];
 }
 
 struct A1D{		// a struct used to create and pass 1D arrays throughout the rest of the program
@@ -17,55 +18,62 @@ struct A1D{		// a struct used to create and pass 1D arrays throughout the rest o
 
 void homeMotorB();
 void homeMotorC();
-void moveAndScan();
 void moveMotorB(int toDist);
 bool scanForColour(Skittle read);
-void spitSkittle(Skittle read);
-void moveAndScan(Skittle colour, bool sort);
+void spitSkittle(Skittle read, A2D config);
+void moveAndScan(Skittle colour, bool sort, A2D config);
 
 task doit(){	// seperate thread used to home the slide motor to save time.
-	wait1Msec(200);
+	wait1Msec(200); // need the delay to start thread. No idea why, but it works.
 	homeMotorB();
 }
 
 
-void reset2DArray(A2D config){  // reset the 2D array to be populted with zeros.
+void reset2DArray(A2D reset){  // reset the 2D array to be populted with zeros.
 	for(int i = 0; i < 5; i++)
 	{
-		for(int j = 0; j < 10; j++)
+		for(int j = 0; j < 13; j++)
 		{
-			config.array[i][j] = 0;
+			reset.array[i][j] = 0;
 		}
-
 	}
 }
 
+void sort(A2D config)
+{
+	Skittle scan;
 
-void config(Skittle returnSkittle){
+}
 
-	A2D raw;
+void config(){
+
+	Skittle returnSkittle;
+	A2D config;
 	TFileHandle fout;
 	bool fileOkay = openWritePC(fout, "CONFIG.txt", 1000); // opening the file, so that the full config can go through, thus never having a blank config
 	for(int k = 0; k < 5; k++){
-		int maxR = 0, maxG = 0, maxB = 0, minR = 0, minG = 0, minB = 0, maxNormal = 0; // values to be put into the config file.
+		int maxR = 0, maxG = 0, maxB = 0, maxSum = 0, minR = 0, minG = 0, minB = 0, minSum = 0, maxNormal = 0; // values to be put into the config file.
 		int normals[5] = {0, 0, 0, 0, 0}; // int for each possible colour
 
 		for(int i = 0; i < 5; i++){ // test a skittle colour and record its RGB values.
 			homeMotorC();
-			moveAndScan(returnSkittle, false);
+			moveAndScan(returnSkittle, false, config);
 			if(maxR < returnSkittle.R) maxR = returnSkittle.R;
-			if(maxG < returnSkittle.R) maxG = returnSkittle.G;
-			if(maxB < returnSkittle.R) maxB = returnSkittle.B;
+			if(maxG < returnSkittle.G) maxG = returnSkittle.G;
+			if(maxB < returnSkittle.B) maxB = returnSkittle.B;
+			if(maxSum < returnSkittle.sum) maxSum = returnSkittle.sum;
 
 			if(minR == 0) minR = maxR;
 			if(minG == 0) minG = maxG;
 			if(minB == 0) minB = maxB;
+			if(minSum == 0) minSum = maxSum;
 
 			if(minR > returnSkittle.R) minR = returnSkittle.R;
 			if(minG > returnSkittle.G) minG = returnSkittle.G;
 			if(minB > returnSkittle.B) minB = returnSkittle.B;
+			if(minSum > returnSkittle.sum) minSum = returnSkittle.sum;
 
-
+			// This part checks to see if the default colour has been recorded already. If so, add it to the list. Otherwise, get rid of it.
 			for(int j = 0; j < 5; j++){
 				if(normals[j] == returnSkittle.normal){
 					j = 5;
@@ -79,18 +87,22 @@ void config(Skittle returnSkittle){
 			}
 
 		}
-
+		// Time to write the data to the text file.
 		writeLongPC(fout, maxR);
 		writeTextPC(fout, " ");
 		writeLongPC(fout, maxG);
 		writeTextPC(fout, " ");
 		writeLongPC(fout, maxB);
 		writeTextPC(fout, " ");
+		writeLongPC(fout, maxSum);
+		writeTextPC(fout, " ");
 		writeLongPC(fout, minR);
 		writeTextPC(fout, " ");
 		writeLongPC(fout, minG);
 		writeTextPC(fout, " ");
 		writeLongPC(fout, minB);
+		writeTextPC(fout, " ");
+		writeLongPC(fout, minSum);
 		writeTextPC(fout, " ");
 		writeLongPC(fout, maxNormal);
 		writeTextPC(fout, " ");
@@ -100,32 +112,40 @@ void config(Skittle returnSkittle){
 		}
 		writeEndlPC(fout);
 		homeMotorC();
-		wait1Msec(2000);
 	}
 
+	closeFilePC(fout);
 }
 
-void loadConfig(A2D loadTo) // load data from the config file and add it nto the 2D array
+void loadConfig(A2D loadTo) // load data from the config file and add it to the 2D array
 {
 	TFileHandle fin;
 	bool okay = openReadPc(fin, "CONFIG.txt");
+	int normals = 0;
 
 	for(int i = 0; i < 5; i++)
 	{
-		for(int j = 0; j < 6; j++){
+		for(int j = 0; j < 8; j++){
 			readIntPC(fin, loadTo.array[i][j]);
 		}
 
+		readIntPC(fin, normals);
+		for(int j = 0; j < normals; j++)
+		{
+			readIntPC(fin, loadTo.array[i][j+8]);
+		}
 
 	}
 
+	closeFilePC(fin);
 }
 
-void moveAndScan(Skittle colour, bool sort){
+void moveAndScan(Skittle colour, bool sort, A2D config){
 	bool done = false;
 	nMotorEncoder[motorC] = 0;
 	int speed = 2;
 	motor[motorC] = speed;
+	// keep on movig the skittle loader until the encoders have gotten far enough.
 	while(!done){
 		if(nMotorEncoder[motorC] > 90){
   		startTask(doit);
@@ -134,16 +154,16 @@ void moveAndScan(Skittle colour, bool sort){
 
  	 	if(SensorValue[S3] != 6){
 			wait1Msec(100);
-			if(SensorValue[S3] != 6) {
+			if(SensorValue[S3] != 6) { // double check colour to remove false positivies
 				if(scanForColour(colour))
 					{
-			 		if(sort)spitSkittle(colour);
+			 		if(sort)spitSkittle(colour, config);
 					eraseDisplay();
 					displayString(0, "%i", colour.R);
 					displayString(1, "%i", colour.G);
 					displayString(2, "%i", colour.B);
 					displayString(3, "%i", colour.normal);
-					displayString(4, "%i", nMotorEncoder[motorC]);
+					displayString(4, "%i", colour.sum);
 					speed = 2;
 					motor[motorC] = speed;
 					wait1Msec(1500);
@@ -155,8 +175,7 @@ void moveAndScan(Skittle colour, bool sort){
 
 }
 
-void spitSkittle(Skittle read){
-
+void spitSkittle(Skittle read, A2D config){
 
 		if(read.normal == 5 && read.R >= 40){
 			eraseDisplay();
@@ -193,7 +212,7 @@ bool scanForColour(Skittle read){
 		while(nMotorEncoder[motorC] <= 45);
 		motor[motorC] = 0;
 		wait1Msec(200);
-		if(sensorValue[S3] != 6){
+		if(SensorValue[S3] != 6){
 			SensorType[S3] = sensorColorNxtRED;
 			wait1Msec(1000);
 			read.R = SensorValue[S3];
@@ -208,6 +227,7 @@ bool scanForColour(Skittle read){
 			SensorType[S3] = sensorColorNxtFULL;
 			wait1Msec(1000);
 			read.normal = SensorValue[S3];
+			read.sum = read.R + read.G + read.B;
 
 			return true;
 		}
@@ -238,18 +258,6 @@ void homeMotorC(){
 	motor[motorC] = 0;
 }
 
-
-
-void testMotorC(){
-	Skittle colour;
-	while(1){
-	homeMotorC();
-	moveAndScan(colour, true);
-
-	}
-
-}
-
 task main()
 {
 	A2D holdConfig;
@@ -263,9 +271,9 @@ task main()
 	wait1Msec(1000);
 
 	//displayStatus();
-	Skittle lol;
-	config(lol);
+	config();
 
+	sort(holdConfig);
 	//testMotorC();
 
 
